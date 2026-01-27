ad08011973f8fb4fde50e0cc9a6815f8c162ae0d
tmostavcuk-boop <tmostavcuk@gmail.com>
2026-01-27 12:36:18 +0200

    Replace FileOpener import with Cordova plugin

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import {
 
 // Додаємо інструменти для Android
 import { Filesystem, Directory } from '@capacitor/filesystem';
-import { FileOpener } from '@cap-browser/file-opener';
+import { FileOpener } from '@awesome-cordova-plugins/file-opener';
 import { jsPDF } from 'jspdf';
 
 // --- Types ---
