dd0e9fc7628fd5f8f86ea4f5302bab2fc01491ef
tmostavcuk-boop <tmostavcuk@gmail.com>
2026-01-27 15:49:48 +0200

    Refactor CSS import and remove inline styles
    Removed inline style reset and added a comment for CSS import.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
-
-// Basic reset styles
-const style = document.createElement('style');
-style.innerHTML = `
-  body { margin: 0; padding: 0; box-sizing: border-box; }
-  * { box-sizing: inherit; }
-`;
-document.head.appendChild(style);
+import './index.css' // Обов'язково імпортуємо стилі!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
